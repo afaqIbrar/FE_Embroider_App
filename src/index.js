12d6e42a6b04ece91e1cf7bf374cc22a7bec77e7
afaqibrar <afaqibrar468@gmail.com>
2024-06-08 14:14:24 +0500

    Date format changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import 'moment/locale/en-gb';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <LocalizationProvider dateAdapter={AdapterMoment}>
+    <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
       <BrowserRouter>
         <App />
       </BrowserRouter>

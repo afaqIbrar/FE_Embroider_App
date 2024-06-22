876ab646de0909ff595b212553e2ae5d62c06e90
afaqibrar <afaqibrar468@gmail.com>
2024-06-22 16:51:53 +0500

    Process Lot Table height

--- src/pages/processLot/index.jsx
+++ src/pages/processLot/index.jsx
@@ -401,7 +401,7 @@ const ProcessLot = () => {
       <Box
         m="8px 0 0 0"
         width="100%"
-        height="640px"
+        height="625px"
         sx={{
           '& .MuiDataGrid-root': {
             border: 'none'
